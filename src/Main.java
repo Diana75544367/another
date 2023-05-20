753594de68455b4ff57dbe238bf6790778a6ac98
Diana <diana.ioana.gherasim@gmail.com>
2023-05-20 12:17:22 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static double devideTwoNumbers(double numberOne, double numberTwo) {
+        double sum = numberOne / numberTwo;
+        return sum;
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        double sum  = devideTwoNumbers( 7, 2);
+        double sum2 = devideTwoNumbers( 8,3);
+        double sum3 = devideTwoNumbers( 4,2);
+System.out.println(sum);
+        System.out.println(sum2);
     }
 }
